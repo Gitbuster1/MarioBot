96ab079d0e259a992afb4022f2079460683d079d
Gitbuster1 <tw_tibi@yahoo.ro>
2021-12-02 14:50:20 +0200

    Modified treshold values and improved image samples for better detection

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ from pynput import keyboard, mouse
 import keyboard as kb
 
 
-def find_object(game_area, obj_img):
+def find_object(game_area, obj_img, threshold):
     result = cv2.matchTemplate(game_area, obj_img, cv2.TM_CCOEFF_NORMED)
     width = obj_img.shape[1]
     height = obj_img.shape[0]
-    # TODO: to be tested more thoroughly to get the best value for the threshold
-    threshold = .55
     y_loc, x_loc = np.where(result >= threshold)
     rectangles = []
     for (x, y) in zip(x_loc, y_loc):
@@ -21,20 +19,20 @@ def find_object(game_area, obj_img):
     return rectangles
 
 
-def check_for_enemy(game_area, enemy_img):
-    rectangles = find_object(game_area, enemy_img)
+def check_for_enemy(game_area, enemy1_img, threshold):
+    rectangles = find_object(game_area, enemy1_img, threshold)
     for (x, y, w, h) in rectangles:
         cv2.rectangle(game_area, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
 
-def check_for_flag(game_area, flag_img):
-    rectangles = find_object(game_area, flag_img)
+def check_for_flag(game_area, flag_img, threshold):
+    rectangles = find_object(game_area, flag_img, threshold)
     for (x, y, w, h) in rectangles:
         cv2.rectangle(game_area, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 
-def check_for_castle(game_area, castle_img):
-    rectangles = find_object(game_area, castle_img)
+def check_for_castle(game_area, castle_img, threshold):
+    rectangles = find_object(game_area, castle_img, threshold)
     for (x, y, w, h) in rectangles:
         cv2.rectangle(game_area, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
@@ -45,15 +43,16 @@ def close_app():
 
 def run():
     key = keyboard.Controller()
-    enemy1_img = cv2.imread("enemy1.png", cv2.IMREAD_UNCHANGED)
-    enemy1_img = cv2.cvtColor(enemy1_img, cv2.COLOR_BGR2GRAY)
-    enemy2_img = cv2.imread("enemy2.png", cv2.IMREAD_UNCHANGED)
-    enemy2_img = cv2.cvtColor(enemy2_img, cv2.COLOR_BGR2GRAY)
-    flag_img = cv2.imread("flag.png", cv2.IMREAD_UNCHANGED)
-    flag_img = cv2.cvtColor(flag_img, cv2.COLOR_BGR2GRAY)
-    castle_img = cv2.imread("castle.png", cv2.IMREAD_UNCHANGED)
-    castle_img = cv2.cvtColor(castle_img, cv2.COLOR_BGR2GRAY)
-    # TODO: change this to a dynamic version, not static (so it can work on multiple devices and screens)
+    enemy1_img = cv2.imread("enemy1.png", cv2.IMREAD_GRAYSCALE)
+    #enemy1_img = cv2.cvtColor(enemy1_img, cv2.COLOR_BGR2GRAY)
+    enemy2_img = cv2.imread("enemy2.png", cv2.IMREAD_GRAYSCALE)
+    enemy2_flipped_img = cv2.imread("enemy2_flipped.png", cv2.IMREAD_GRAYSCALE)
+    #enemy2_img = cv2.cvtColor(enemy2_img, cv2.COLOR_BGR2GRAY)
+    flag_img = cv2.imread("flag.png", cv2.IMREAD_GRAYSCALE)
+    #flag_img = cv2.cvtColor(flag_img, cv2.COLOR_BGR2GRAY)
+    castle_img = cv2.imread("castle.png", cv2.IMREAD_GRAYSCALE)
+    #castle_img = cv2.cvtColor(castle_img, cv2.COLOR_BGR2GRAY)
+    # TODO: change this to a dynamic version, not static (so it can work on different devices and screens)
     with mss.mss() as mss_instance:
         monitor = mss_instance.monitors[2]
         monitor.__init__({'top': 31, 'left': 3168, 'width': 672, 'height': 672})
@@ -62,10 +61,11 @@ def run():
         game_area = np.array(screenshot)
         game_area = cv2.cvtColor(game_area, cv2.COLOR_BGR2GRAY)
 
-        check_for_enemy(game_area, enemy1_img)
-        check_for_enemy(game_area, enemy2_img)
-        check_for_flag(game_area, flag_img)
-        check_for_castle(game_area, castle_img)
+        check_for_enemy(game_area, enemy1_img, threshold=0.46)
+        check_for_enemy(game_area, enemy2_img, threshold=0.46)
+        check_for_enemy(game_area, enemy2_flipped_img, threshold=0.53)
+        check_for_flag(game_area, flag_img, threshold=0.55)
+        # check_for_castle(game_area, castle_img, threshold=0.5)
 
         cv2.imshow('Screen', game_area)
         cv2.waitKey(1)
